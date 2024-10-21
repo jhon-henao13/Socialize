60e0b0ecc5cbebb8f833afd1fd875085208383e9
Estiguar <jhonestiwarhenao@gmail.com>
2024-10-21 17:41:03 -0500

    modificacion de variables de config.php para Railway

--- app/config/config.php
+++ app/config/config.php
@@ -2,15 +2,15 @@
 
 // Definimos las constantes de nuestra base de datos
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'redsocial');
+define('DB_HOST', 'mysql.railway.internal');
+define('DB_NAME', 'railway');
 define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'mSrOAYHiBbqaREeqfdTmCLBuROMUIflM');
 
 // Definir las constantes de nuestro proyecto
 
 define('URL_APP', dirname(dirname(__FILE__)));
-define('URL_PROJECT', 'http://localhost/red-social/');
+define('URL_PROJECT', 'https://socialize-production-9017.up.railway.app/');
 
 define('NAME_PROJECT', 'Socialize');
 
